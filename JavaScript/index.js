74b5b58923990559a2c714938bba51f6aa11f290
saujalkarki <saujalkarki04@gmail.com>
2023-10-12 09:06:20 +0545

    learning JavaScript Basics

---
+++ JavaScript/index.js
@@ -0,0 +1,57 @@
+console.log("My name is Saujal karki");
+
+// task 1
+
+let num1 = "526";
+let num2 = "235";
+
+let total = num1 + num2;
+
+console.log(total);
+
+let ab = 5;
+let cd = 5;
+let ef = ab * cd;
+
+console.log(ef);
+
+// club task
+let age1 = 21;
+let age2 = 18;
+let age3 = 25;
+
+if (age1 >= 21) {
+  console.log("You can have some drink.");
+} else {
+  console.log("You cannot drink here.");
+}
+
+if (age2 >= 21) {
+  console.log("You can have some drink.");
+} else {
+  console.log("You cannot drink here.");
+}
+
+if (age3 >= 21) {
+  console.log("You can have some drink.");
+} else {
+  console.log("You cannot drink here.");
+}
+
+// id card
+let have_id1 = true;
+let have_id2 = false;
+
+if (have_id1) {
+  console.log("You can enter the school.");
+} else {
+  console.log("You cannot enter the school.");
+}
+
+if (have_id2) {
+  console.log("You can enter the school.");
+} else {
+  console.log("You cannot enter the school.");
+}
+
+// data_Types, conditionlas, null, undefined, NaN
