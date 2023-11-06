8d0d44ab6040e9b5772be2b9cdef2b223d80efa2
Saujal Karki <saujalkarki04@gmail.com>
2023-11-06 09:02:11 +0545

    about props and state

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
+
 import { ClassComponent, FunctionComponent } from "./components";
 
 // Data flow top-down, one-way
 function App() {
+  const [display, setDisplay]=useState(true);
+
   return (
     <>
-      <FunctionComponent>
+      <FunctionComponent user={{name:"John",age:20}} displayChildren={false} country="Nepal" cars={["Thar", "scorpio"]} salary={10_00_000} users={true}> 
         <ClassComponent>
           <h2>Children</h2>
         </ClassComponent>
@@ -12,6 +16,7 @@ function App() {
           <h2>Children</h2>
         </ClassComponent>
       </FunctionComponent>
+      <button onClick={()=>setDisplay(!display)}>Display</button>
     </>
   );
 }
--- React/src/components/FunctionComponent.jsx
+++ React/src/components/FunctionComponent.jsx
@@ -1,9 +1,33 @@
-export function FunctionComponent({ children }) {
-  return (
-    <>
-      <h1>Function Component</h1>
-      {children}
-      <h1>Function Component</h1>
-    </>
-  );
+import { useState } from "react";
+
+export function FunctionComponent({user={name:"John", age:20}, country, salary, cars, children, displayChildren, users }) {
+  console.log(user, country, salary, cars)
+  const [count, setCount]= useState(1);
+
+  // if(displayChildren){
+  //   return children;
+  // }else{
+  //   return (
+  //     <>
+  //       <h1>I am {user.name}</h1>
+  //       <h2 >Count: {count}</h2>
+  //       {children}
+  //       <h1>Function Component</h1>
+  //       <button onClick={()=>setCount(2)}>Change to 2</button>
+  //     </>
+  //   );
+  // }
+
+  return displayChildren?(children):(
+       <>
+        {users && <h1>I'm {user.name}</h1>}
+        <h2 >Count: {count}</h2>
+        {children}
+        <h1>Function Component</h1>
+         <button onClick={()=>setCount(2)}>Change to 2</button>
+      </>
+     );
 }
+
+// We can export by this way too
+// export default FunctionComponent;
