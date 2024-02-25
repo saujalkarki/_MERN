8abc6c89220d38203674a43a969caa30e381bb87
Saujal Karki <saujalkarki04@gmail.com>
2024-02-25 23:58:02 +0545

    model

---
+++ CMS/model/blogModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    Subtitle: String,
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Blog", blogSchema);
